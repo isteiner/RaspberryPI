ba6acf599099b243319a243a2c019ba7f02cf837
isteiner <isteiner067@gmail.com>
2015-07-26 23:05:40 +0200

    Change shell

--- Cosm/xively.sh
+++ Cosm/xively.sh
@@ -1,5 +1,6 @@
-#!/usr/bin/python
+#!/bin/bash
 
 # Start the python module for gathering sensor data and routing them to Pachube
-sudo python /home/pi/pachube/pachube_post.py
+python /home/pi/pachube/pachube_post.py
+sudo reboot
 
